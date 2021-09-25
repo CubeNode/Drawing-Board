ea877a0c57514d80235b923ba543ba03f6e42b55
Clint Jones <34784858+CubeNode@users.noreply.github.com>
2021-09-25 19:51:12 -0400

    Add files via upload
    Added new buttons and touch events.

--- index.js
+++ index.js
@@ -9,8 +9,21 @@ coord = {x:0, y:0},
 color = '#000',
 width = 1;
 
-let erase = () => {
+let red = 0,
+green = 0,
+blue = 0;
+
+let clear = () => {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+let fillCanvas = () => {
+  if(document.getElementById("fill-toggle").checked) {
+    ctx.fillStyle = 'rgb(' + red + ', ' + green + ', ' + blue + ')';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+  }
+
+  document.getElementById("fill-toggle").checked = false;
 }
 
 let startPainting = (e) => {
@@ -23,18 +36,33 @@ let stopPainting = () => {
 }
 
 let painting = (e) => {
+
   if(paint){
 
-    ctx.beginPath();
-    ctx.moveTo(coord.x, coord.y);
+    if(document.getElementById("erase-toggle").checked) {
+      ctx.beginPath();
+      ctx.moveTo(coord.x, coord.y);
 
-    getMousePos(e);
+      getMousePos(e);
 
-    ctx.lineTo(coord.x, coord.y);
-    ctx.strokeStyle = document.getElementById("color-picker").value;
-    ctx.lineCap = "round";
-    ctx.lineWidth = document.getElementById("width-picker").value;
-    ctx.stroke();
+      ctx.lineTo(coord.x, coord.y);
+      ctx.strokeStyle = "#FFF";
+      ctx.lineCap = "round";
+      ctx.lineWidth = document.getElementById("width-picker").value;
+      ctx.stroke();
+    } else {
+      ctx.beginPath();
+      ctx.moveTo(coord.x, coord.y);
+
+      getMousePos(e);
+
+      ctx.lineTo(coord.x, coord.y);
+      ctx.strokeStyle = 'rgb(' + red + ', ' + green + ', ' + blue + ')';
+      ctx.lineCap = "round";
+      ctx.lineWidth = document.getElementById("width-picker").value;
+      ctx.stroke();
+    }
+
   }
 
 }
@@ -49,6 +77,22 @@ let updateVal = (val) => {
 
   label.innerHTML = "Width: " + val;
 }
+
+let colors = () => {
+  red = document.getElementById("red").value;
+  green = document.getElementById("green").value;
+  blue = document.getElementById("blue").value;
+  document.getElementById("output").innerHTML = 'rgb(' + red + ',' + green + ',' + blue + ')';
+  document.getElementById("preview").style.backgroundColor = 'rgb(' + red + ',' + green + ',' + blue + ')';
+}
+
+canvas.addEventListener("touchstart", startPainting);
+
+canvas.addEventListener("touchend", stopPainting);
+
+canvasa.addEventListener("touchmove", painting);
+
+canvas.addEventListener("click", fillCanvas);
 
 canvas.addEventListener("mousedown", startPainting);
 
