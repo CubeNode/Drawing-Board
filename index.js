7a4fc7dfc4f9bfb4e097194bf8076253cc8dfdf6
Clint Jones <34784858+CubeNode@users.noreply.github.com>
2021-09-25 22:19:35 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -93,29 +93,11 @@ let colors = () => {
   document.getElementById("preview").style.backgroundColor = 'rgb(' + red + ',' + green + ',' + blue + ')';
 }
 
-canvas.addEventListener("touchstart", function (e) {
-        coord = getTouchPos(canvas, e);
-  var touch = e.touches[0];
-  var mouseEvent = new MouseEvent("mousedown", {
-    clientX: touch.clientX,
-    clientY: touch.clientY
-  });
-  canvas.dispatchEvent(mouseEvent);
-}, false);
+canvas.addEventListener("touchstart", startPainting, false);
 
-canvas.addEventListener("touchend", function (e) {
-  var mouseEvent = new MouseEvent("mouseup", {});
-  canvas.dispatchEvent(mouseEvent);
-}, false);
+canvas.addEventListener("touchend", stopPainting, false);
 
-canvas.addEventListener("touchmove", function (e) {
-  var touch = e.touches[0];
-  var mouseEvent = new MouseEvent("mousemove", {
-    clientX: touch.clientX,
-    clientY: touch.clientY
-  });
-  canvas.dispatchEvent(mouseEvent);
-}, false);
+canvas.addEventListener("touchmove", painting, false);
 
 canvas.addEventListener("click", fillCanvas);
 
